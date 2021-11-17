7aa84e6d7b05450f1217065fbbdaf2e0a929a037
IHomov <innapariy@gmail.com>
2021-11-17 18:45:50 +0200

    diapason from 100 to 999
    fixed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let aHoundred = [ " ", "сто", "двісті", "триста", "чотириста", "п'ятсот", "шістьсот", "сімсот", "вісімсот", "дев'ятьсот"];
 
-let aTens = [" ", "", "двадцять", "тридцять", "сорок", "п'ятьдесят", "шістьдесят", "сімдесят", "вісімдесят", "дев'яносто"];
+let aTens = ["", "", "двадцять", "тридцять", "сорок", "п'ятьдесят", "шістьдесят", "сімдесят", "вісімдесят", "дев'яносто"];
 
 let aOnes = [" ", "одна", "дві", "три", "чотири", "п'ять", "шість", "сім", "вісім", "дев'ять", "десять", "одинадцять", "дванадцять", "тринадцять", "чотирнадцять", "п'ятнадцять", "шістьнадцять", "сімнадцять", "вісімнадцять", "дев'ятнадцять"];
 
@@ -9,7 +9,7 @@ function convertnumbertostring(){
     let num = document.getElementById("n").value;    
     let res = document.getElementById("tekst");    
     let j = 4;
-        if( num.length < j ){
+    if( num.length < j ){
                 let y = ConvertToWords(num);
                 let hrivna = Hrn(num);
             res.innerHTML = `Сума: ${y} ${hrivna} `;
@@ -18,40 +18,57 @@ function convertnumbertostring(){
         }
 };
     
-    function ConvertToWords(num) {
-            let out = 0;
-            let inputNumber = num.split('');
+function ConvertToWords(num) {
+        let out = 0;
+        let inputNumber = num.split('');
         
     if (num.length == 1) { // числа от 1 то 9 включительно
         out = aOnes[num];
         return out;
     }
 
-    if (num.length == 2) { // проверка на 2-х значное число
+        if (num.length == 2) {
+         // проверка на 2-х значное число
         
             // проверка на диапазон от 10 вкл. до 19 вкл-но
-        if (num >= 10 && num < 20) {
-                out = aOnes[num];
-                return out;
-            }
-            // диапазон от 20 до 99
-        
-        else if (num >= 20 && num <= 99) {
-            if (inputNumber[1] != 0) {
-                out = aTens[inputNumber[0]] + " " + aOnes[inputNumber[1]];
-            }
-            else {
-                out = aTens[inputNumber[0]];
-            }
-                return out;
-            }
+                if (num >= 10 && num < 20) {
+                        out = aOnes[num];
+                        return out;
+                    }
+                    // диапазон от 20 до 99
+                
+                else if (num >= 20 && num <= 99) {
+                        if (inputNumber[1] != 0) {
+                            out = aTens[inputNumber[0]] + " " + aOnes[inputNumber[1]];
+                        }
+                        else {
+                            out = aTens[inputNumber[0]];
+                        }
+                            return out;
+                        }
         }
+
+
         //  диапазон от 100 до 999
-        // else {
-        //     out = aHoundred[inputNumber[0]] + " " + aTens[inputNumber[1]] + " " + aOnes[inputNumber[2]];
-        //     return out;
+        else {
+            let last = num.slice(1); // получаем 2 последние цифры
+            let sum = Number(last);
+            if (sum < 20) { // проверка 2-х последних чисел из 3-х значного числа на меньше 20
+                out = aHoundred[inputNumber[0]] + " " + aOnes[sum];
+                
+            } 
+            else { 123
+                if (inputNumber[2] != 0) {
+                            out = aHoundred[inputNumber[0]] + " " + aTens[inputNumber[1]] + " " + aOnes[inputNumber[2]];
+                        }
+                        else {
+                            out = aHoundred[inputNumber[0]] + " " + aTens[inputNumber[2]] ;
+                        }
+            }
+    
+            return out;
         
-        // }
+        }
 
 }
 function Hrn(n) {
